c48aba8e15692019838e0bd00fd38f9119166f7b
apxllo1 <bravounlimitaura@gmail.com>
2026-05-09 10:45:32 -0500

    Add Misc component visibility to Pages

--- src/views/Pages/Pages.tsx
+++ src/views/Pages/Pages.tsx
@@ -5,17 +5,21 @@ import { useCurrentPage } from "hooks/use-current-page";
 import { DashboardPage } from "store/models/dashboard.model";
 import Apps from "./Apps";
 import Home from "./Home";
+import Misc from "./Misc";
 import Options from "./Options";
 import Scripts from "./Scripts";
 
 function Pages() {
 	const currentPage = useCurrentPage();
 	const isScriptsVisible = useDelayedUpdate(currentPage === DashboardPage.Scripts, 2000, (isVisible) => isVisible);
+	const isMiscVisible = useDelayedUpdate(currentPage === DashboardPage.Misc, 2000, (isVisible) => isVisible);
+
 	return (
 		<>
 			<Home Key="home" />
 			<Apps Key="apps" />
 			{isScriptsVisible && <Scripts Key="scripts" />}
+			{isMiscVisible && <Misc Key="misc" />}
 			<Options Key="options" />
 		</>
 	);
