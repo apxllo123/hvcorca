15eab949387bd30b05854302038999f67b641707
apxllo123 <danwantstocode@outlook.com>
2026-05-10 08:50:28 -0500

    Delete ci/minify.js

--- ci/minify.js
+++
@@ -1,6 +0,0 @@
-const luamin = require("luamin");
-const path = require("path");
-const fs = require("fs");
-
-const result = luamin.minify(fs.readFileSync(path.join(__dirname, "/bundle.tmp"), "utf8"));
-fs.writeFileSync("ci/bundle.tmp", result);
